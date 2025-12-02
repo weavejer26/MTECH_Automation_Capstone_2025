8b3e61ec75bce725a78073386feb20fbef45ca9d
Jeremy Weaver <weavejer26@gmail.com>
2025-12-02 13:47:37 -0700

    Added rest of products to Featured Products Test

--- test/pageobjects/FeaturedProductsSelectors.js
+++ test/pageobjects/FeaturedProductsSelectors.js
@@ -23,6 +23,106 @@ class FeaturedSelectors extends Website {
         await this.connectorsProduct.click();
         await browser.back();
     }
+
+    get transistorsProduct () {
+        return $('a[href="/collections/transistors"]')
+    }
+
+    async transistorsProductlink () {
+        await this.transistorsProduct.waitForDisplayed();
+        await this.transistorsProduct.click();
+        await browser.back();
+    }
+
+    get cablesProduct () {
+        return $('a[href="/collections/cable"]')
+    }
+
+    async cablesProductLink () {
+        await this.cablesProduct.waitForDisplayed();
+        await this.cablesProduct.click();
+        await browser.back();
+    }
+
+    get heatshrinktubingProduct () {
+        return $('a[href="/collections/heat-shrink-tubing-1"]')
+    }
+
+    async heatshrinktubingProductLink () {
+        await this.heatshrinktubingProduct.waitForDisplayed();
+        await this.heatshrinktubingProduct.click();
+        await browser.back();
+    }
+
+    get diodesProduct () {
+        return $('a[href="/collections/diodes-1"]')
+    }
+
+    async diodesProductLink () {
+        await this.diodesProduct.waitForDisplayed();
+        await this.diodesProduct.click();
+        await browser.back();
+    }
+
+    get batteriesProduct () {
+        return $('a[href="/collections/battery"]')
+    }
+
+    async batteriesProductLink () {
+        await this.batteriesProduct.waitForDisplayed();
+        await this.batteriesProduct.click();
+        await browser.back();
+    }
+
+    get fusesProduct () {
+        return $('a[href="/collections/fuses-1"]')
+    }
+
+    async fusesProductLink () {
+        await this.fusesProduct.waitForDisplayed();
+        await this.fusesProduct.click();
+        await browser.back();
+    }
+
+    get toolsProduct () {
+        return $('a[href="/collections/tools-2"]')
+    }
+
+    async toolsProductLink () {
+        await this.toolsProduct.waitForDisplayed();
+        await this.toolsProduct.click();
+        await browser.back();
+    }
+
+    get capacitorsProduct () {
+       return $('a[href="/collections/capacitor"]')
+    }
+
+    async capacitorsProductLink () {
+        await this.capacitorsProduct.waitForDisplayed();
+        await this.capacitorsProduct.click();
+        await browser.back();
+    }
+
+    get bananaplugProduct () {
+        return $('a[href="/collections/banana-plug"]')
+    }
+
+    async bananaplugProductLink() {
+        await this.bananaplugProduct.waitForDisplayed();
+        await this.bananaplugProduct.click();
+        await browser.back();
+    }
+
+    get resistorsProduct () {
+        return $('a[href="/collections/resistors-1"]')
+    }
+
+    async resistorsProductLink () {
+        await this.resistorsProduct.waitForDisplayed();
+        await this.resistorsProduct.click();
+        await browser.back();
+    }
 };
 
 export default new FeaturedSelectors();
--- test/specs/FeaturedProductsTest.js
+++ test/specs/FeaturedProductsTest.js
@@ -2,9 +2,20 @@ import FeaturedProductsSelectors from '../pageobjects/FeaturedProductsSelectors.
 describe('Automated SSE Capstone Test (Featured Products)', () => {
 
 //Featured Products
-    it('Should Open Each Featured Product Page and return to Home when Clicked On', async () => {
+    it('Should Open Each Featured Product Page When Clicked On And Return To Home Page Afterwards', async () => {
         await FeaturedProductsSelectors.open();
         await FeaturedProductsSelectors.solderingProductLink();
         await FeaturedProductsSelectors.connectorsProductLink();
+        await FeaturedProductsSelectors.transistorsProductlink();
+        await FeaturedProductsSelectors.cablesProductLink();
+        await FeaturedProductsSelectors.heatshrinktubingProductLink();
+        await FeaturedProductsSelectors.diodesProductLink();
+        await FeaturedProductsSelectors.batteriesProductLink();
+        await FeaturedProductsSelectors.fusesProductLink();
+        await FeaturedProductsSelectors.toolsProductLink();
+        await FeaturedProductsSelectors.capacitorsProductLink();
+        await FeaturedProductsSelectors.bananaplugProductLink();
+        await FeaturedProductsSelectors.resistorsProductLink();
+
     })
 });
